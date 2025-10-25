6118fb072b4434da2f1ddc9878b79958c8b26d13
Jordan Mark Barbone <jmbarbone@gmail.com>
2025-10-25 18:50:11 -0400

    adds more bench marks

--- bench/environment.R
+++ bench/environment.R
@@ -18,7 +18,6 @@ bench::mark(
   })() |>
   ggplot2::autoplot()
 
-
 nm <- sample(names(e), 100)
 
 bench::mark(
@@ -28,6 +27,49 @@ bench::mark(
   as.list(e), # pretty okay
   mget(names(e), e), # terrible
   check = FALSE
+) |>
+  (\(x) {
+    print(x[c("expression", "min", "median", "itr/sec", "mem_alloc", "n_itr")])
+    invisible(x)
+  })() |>
+  ggplot2::autoplot()
+
+bench::mark(
+  hashed_mget = mget(nms, e1), # third
+  # unhashed_mget = mget(nms, e2), # this is just really bad
+  hashed_as_list_ext = as.list(e1)[nms], # second
+  unhashed_as_list_ext = as.list(e2)[nms], # first
+  # hashed_as_list = as.list(e1, sorted = TRUE), # pretty bad
+  # unhashed_as_list = as.list(e2, sorted = TRUE), # pretty bad
+  check = TRUE,
+  env = local({
+    n <- runif(1e5)
+    nms <- sort(as.character(n))
+    ls <- structure(as.list(n), names = nms)
+    e1 <- list2env(ls, hash = TRUE)
+    e2 <- list2env(ls, hash = FALSE)
+    environment()
+  })
+) |>
+  (\(x) {
+    print(x[c("expression", "min", "median", "itr/sec", "mem_alloc", "n_itr")])
+    invisible(x)
+  })() |>
+  ggplot2::autoplot()
+
+
+bench::mark(
+  try(mget("fail", e1)),
+  try(mget("fail", e2)), # obviously better
+  check = FALSE,
+  env = local({
+    n <- runif(1e4)
+    nms <- sort(as.character(n))
+    ls <- structure(as.list(n), names = nms)
+    e1 <- list2env(ls, hash = TRUE)
+    e2 <- list2env(ls, hash = FALSE)
+    environment()
+  })
 ) |>
   (\(x) {
     print(x[c("expression", "min", "median", "itr/sec", "mem_alloc", "n_itr")])
