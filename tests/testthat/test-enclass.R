24588294f2b3bc8125eee5c607331d165e607841
Jordan Mark Barbone <jmbarbone@gmail.com>
2025-10-26 15:33:00 -0400

    adds basic testing

---
+++ tests/testthat/test-enclass.R
@@ -0,0 +1,43 @@
+test_that("enclass() works", {
+  Foo := enclass({
+    current <- 0L
+    do <- function(x = 1L) {
+      x <- as.integer(x)
+      self@current <- self@current * x
+      self@.record <- c(
+        self@.record,
+        list(list(time = Sys.time(), value = self@current))
+      )
+      invisible(self)
+    }
+
+    .record <- list()
+
+    .__new__. <- function(start = 0L) {
+      self$do(start)
+    }
+  })
+
+  foo <- Foo()
+  expect_identical(foo@current, 0L)
+  foo$do(5L)
+  expect_identical(foo@current, 0L)
+})
+
+test_that("enclass() errors", {
+  Foo := enclass({
+    a <- 1
+    b <- function() 2
+  })
+
+  expect_error(Foo@aa)
+  expect_error(Foo$bb())
+  expect_error(Foo$a())
+  expect_error(Foo@b)
+
+  foo <- Foo()
+  expect_error(foo@aa)
+  expect_error(foo$bb())
+  expect_error(foo$a())
+  expect_error(foo@b)
+})
